5213dea9f74e0dad83a31a4de5f0f768221d5b94
Luna <luna.zehnder@laptop.nix>
2024-11-14 22:12:59 +0100

    started game logic

--- modules/gameLogic.js
+++ modules/gameLogic.js
@@ -1,3 +1,5 @@
+import PlayingField from "./playingField.js";
+
 export default class GameLogic {
   constructor(sceneInformation) {
     this.sceneInformation = sceneInformation;
@@ -19,9 +21,13 @@ export default class GameLogic {
     // Settings
     let tickIntervall = 1/60 * 1000;
 
+    this.playingField = new PlayingField(this.sceneInformation);
+
     // Initial Game State
     this.health = 10;
     this.money = 1000.0;
+    this.waveCounter = 0;
+    this.score = 0;
 
     this.currentGame = setInterval(this.updateGame, tickIntervall);
   }
@@ -38,5 +44,9 @@ export default class GameLogic {
 
   onMouseClick() {
     this.stopGame();
+  }
+
+  startNewWave() {
+    this.waveCounter += 1;
   }
 }
---
+++ modules/playingField.js
@@ -0,0 +1,32 @@
+export default class PlayingField {
+  constructor(sceneInformation) {
+    this.sceneInformation = sceneInformation;
+    this.tileCount = 0;
+    this.startTile = this.createNewTile("start", 0, [0, 0, 0]);
+  }
+
+  createNewTile(type, rotation, coordinates) {
+    const newTile = {
+      inWorldTile: this.sceneInformation.addNewObject(
+        "hexagonal-plate-" + type,
+        coordinates,
+        [0, rotation, 0], 1, texture),
+      type: type,
+      rotation: rotation,
+      coordinates: coordinates,
+      neighbors: findNeighbors(coordinates),
+      id: this.tileCount++,
+    }
+
+
+    return newTile;
+  }
+
+  findNeighborsOfTile(coordinates) {
+    const neighbors = [];
+    // ToDo find neighbors
+    
+    return neighbors;
+  }
+  
+}
