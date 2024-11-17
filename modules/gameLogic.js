27af1d4d71b924c941c7248296375553fcf103fa
cactus <cactus@feather>
2024-11-17 17:26:42 +0100

    game overlay, triggered on 0 health

--- modules/gameLogic.js
+++ modules/gameLogic.js
@@ -2,7 +2,7 @@ import PlayingField from "./playingField.js";
 import EnemyHandler from "./enemyHandler.js";
 import TowerHandler from "../models/towerHandler.js";
 import Shop from "./shop.js";
-import { setHealth, setWaveProgress, setWealth } from "./uiHook.js";
+import { setHealth, setWaveProgress, setWealth, toggleGameOverlay } from "./uiHook.js";
 import { generateEnemyPattern } from "./waveLogic.js";
 
 export default class GameLogic {
@@ -30,7 +30,7 @@ export default class GameLogic {
     this.towerHandler = new TowerHandler(this.sceneInformation, this.playingField, this.enemyHandler);
     this.shop = new Shop(this.playingField, this.sceneInformation, this.towerHandler);
     // Initial Game State
-    this.maxHealth = 10;
+    this.maxHealth = 1;
     this.health = this.maxHealth;
     this.money = 1000.0;
     this.maxMoney = Math.max(this.money);
@@ -54,6 +54,10 @@ export default class GameLogic {
   updateGame() {
     let dmg = this.enemyHandler.doTick();
     this.health -= dmg;
+    if (this.health <= 0) {
+      this.health = 0;
+      toggleGameOverlay(true);
+    }
     this.towerHandler.doTick();
     this.money = this.shop.money;
     this.maxMoney = Math.max(this.money, this.maxMoney);
